faeaeba94454c52c245c2dd5861dee7381152514
Shoeb rehman <rehmanbshoeb@gmail.com>
2016-03-31 18:51:11 -0400

    added mongo

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ Created on Wed Mar 30 14:39:59 2016
 @author: Shoeb
 """
 
-from NeoSandbox import loadFromFiles,findTrusted,deleteAll,findCommon
+from databaseSandbox import loadFiles,findTrusted,deleteAll,findCommon
+from mongoSandbox import lookUpUser
+
 def menu():
     while(True):
-        choice = raw_input('''1. Load From Files\n2. Find Individuals with Similar Interests\n3. Find Trusted Colleagues of Colleagues\n4. Query a User via userID\n5. Clear Database\n6. Exit\nChoice: ''')
+        choice = raw_input('''1. Load From Files\n2. Find Individuals with Similar Interests\n3. Find Trusted Colleagues of Colleagues\n4. Query a User via userID\n5. Clear Databases\n6. Exit\nChoice: ''')
         if(choice == '1'):
             csvFiles = ['names.csv','organizations.csv','projects.csv','interests.csv','skills.csv','distance.csv']
-            loadFromFiles(csvFiles)
+            loadFiles(csvFiles)
         if(choice == '2'):
             userID = int(raw_input('Enter userID you wish you query for: '))
             findCommon(userID)
         if(choice == '3'):
             userID = int(raw_input('Enter userID you wish you query for: '))
             findTrusted(userID)
+        if(choice == '4'):
+            userID = raw_input('Enter userID you wish you query for: ')
+            lookUpUser(userID)
         if(choice == '5'):
             choice = raw_input('Are you sure? y/n ')
             if(choice == 'y'):
@@ -26,4 +31,3 @@ def menu():
             return False
 
 menu()
-    
---
+++ mongoSandbox.py
@@ -0,0 +1,90 @@
+import pymongo, csv
+
+client = pymongo.MongoClient()
+db = client["projectOne"]
+user= db["Users"]
+organizations= db["Organizations"]
+interests= db["Interests"]
+projects= db["Projects"]
+skill= db["Skill"]
+
+def main():
+    client = pymongo.MongoClient()
+    db = client["projectOne"]
+
+    #reads data from file as input from the user provided
+    user= db["Users"]
+    organizations= db["Organizations"]
+    interests= db["Interests"]
+    projects= db["Projects"]
+    skill= db["Skill"]
+    while True:
+        val = raw_input("\nWould you like to search for a user (y/n):  ")
+        if val == 'y':
+            lookUpUser(user, organizations, interests, skill, projects)
+        else:
+            break
+
+
+def lookUpUser(userID):
+    if((user.find({"userID": str(userID)})).count() == 0):
+        print "This user does not exist. Please try another userID."
+
+    else:
+        user_results = user.find({"userID": str(userID)})   
+        organization_result = organizations.find({"userID": str(userID)})
+        interests_results = interests.find({"userID": str(userID)})
+        skill_results = skill.find({"userID": str(userID)})
+        projects_results = projects.find({"userID": str(userID)})
+        
+        for u in user_results:
+            print("\tName: %s %s" %(u['fName'], u['lName']))
+    
+        for o in organization_result:
+            print("\tWorks at: %s" %o["organization"])
+    
+        interests_collection = ""
+        
+        for i in interests_results:
+            interests_collection += str(i['interest']) + "(" +str(i['Interest level']) + "),"
+        print("\tInterests: %s" % interests_collection)
+    
+        skill_collection = ""
+        
+        for sk in skill_results:
+             skill_collection += str(sk['skill']) + "(" +str(sk['skill_level']) + "),"
+        print("\tSkill : %s" %skill_collection)    
+    
+        for p in projects_results:
+            print("\tWorks on: %s" %p["project"])
+
+
+def readFile(file_name):
+    r_file = open(file_name)
+    csv_file = csv.reader(r_file)
+    
+    headers = next(csv_file)
+
+
+    keys = []
+    data_entry = {}
+    size = 0
+    data_collection = []
+
+    for key in headers:
+        keys.append(key)
+
+    size = len(keys)
+    for rows in csv_file:
+        for i in range(size):
+            data_entry[keys[i]] = rows[i]
+        data_collection.append(data_entry)
+        data_entry = {}
+
+    r_file.close()
+    return data_collection
+
+
+def insertData(collection_name, entries):
+    for data in entries:
+        collection_name.insert_one(data)
