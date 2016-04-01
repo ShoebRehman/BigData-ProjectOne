c351a49b7978fe4c1e5427116e71a585b5bea9ef
Shoeb rehman <rehmanbshoeb@gmail.com>
2016-04-01 13:53:30 -0400

    imported new functions created

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ Created on Wed Mar 30 14:39:59 2016
 @author: Shoeb
 """
 
-from databaseSandbox import loadFiles,findTrusted,deleteAll,findCommon
+from Neo4JSandbox import findTrusted,findCommon
 from mongoSandbox import lookUpUser
+from database import loadData,deleteAll
 
 def menu():
     while(True):
         choice = raw_input('''1. Load From Files\n2. Find Individuals with Similar Interests\n3. Find Trusted Colleagues of Colleagues\n4. Query a User via userID\n5. Clear Databases\n6. Exit\nChoice: ''')
         if(choice == '1'):
             csvFiles = ['names.csv','organizations.csv','projects.csv','interests.csv','skills.csv','distance.csv']
-            loadFiles(csvFiles)
+            loadData(csvFiles)
         if(choice == '2'):
             userID = int(raw_input('Enter userID you wish you query for: '))
             findCommon(userID)
